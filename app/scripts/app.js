f5509e427a033271b7bb8003b754a739fc07e906
konamgil <rhskarlf6461@gmail.com>
2015-11-22 12:56:22 +0900

    generate todoList

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myToDoApp
+ * @description
+ * # myToDoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myToDoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable'
+  ])
+  .config(function ($routeProvider) {
+    //MainCtrl, main.html 설정을 변경한다.
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/todo.html',
+        controller: 'TodoCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
